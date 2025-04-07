2b1e729038b7152bc92c0c8a6269b59fecf9a1e9
rflrytdzn <kuhaku.blank.rd@gmail.com>
2025-04-07 10:36:36 +0800

    updated ui

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,112 +1,56 @@
 import Team from "../assets/about.jpg";
 import Roy from "../assets/2x2.jpg";
 import Report from "../assets/report1.png"
-import Pic2 from "../assets/pic2.png"
+import Pic2 from "../assets/milis.png"
+import Prototype from "../assets/prototype.png"
+import Prototype2 from "../assets/prototype2.png"
+import ml from "../assets/ml2.png"
+import arduino from "../assets/arduino.png"
+import seng from "../assets/seng.jpg"
+import enrico from "../assets/enrico.jpg"
 
 
 function About() {
   return (
     <div>
-      {/* <img src={Team} className="img-fluid height-50"></img> */}
-
+      <div className="d-flex justify-content-between align-items-center">
+      <img src={Prototype2} className=" " height="500"></img>
+        <div className="text-center">
+        <h2 className=" fw-bold" style={{fontSize:'7vw'}}>Apnea-lyze Pro</h2>
+        <p>An affordable, automated solution for obstructive sleep apnea—where 
+          cutting-edge hardware and intelligent AI work seamlessly to deliver precise, 
+          at-home sleep analysis.</p>
+          <button className="btn btn-outline-dark fs-4">Start OSA Test</button>
+        </div>
+      
+      <img src={ml} className="" height="400"></img>
+      </div>
       <div class="container marketing">
-        {/* <div class="row text-bg-dark rounded-3 text-center">
-          <div class="col-lg-4 ">
-            <img src={Roy} 
-            className=" rounded-circle w-50" />
-              <title>Placeholder</title>
-              <rect
-                width="100%"
-                height="100%"
-                fill="var(--bs-secondary-color)"
-              />
-            
-            <h2 class="fw-normal">Heading</h2>
-            <p>
-              Some representative placeholder content for the three columns of
-              text below the carousel. This is the first column.
-            </p>
-            <p>
-              <a class="btn btn-secondary" href="#">
-                View details &raquo;
-              </a>
-            </p>
-          </div>
-          <div class="col-lg-4">
-            
-                <img src={Roy} 
-            className=" rounded-circle w-50" />
-              <title>Placeholder</title>
-              <rect
-                width="100%"
-                height="100%"
-                fill="var(--bs-secondary-color)"
-              />
-            
-            <h2 class="fw-normal">Heading</h2>
-            <p>
-              Another exciting bit of representative placeholder content. This
-              time, we've moved on to the second column.
-            </p>
-            <p>
-              <a class="btn btn-secondary" href="#">
-                View details &raquo;
-              </a>
-            </p>
-          </div>
-          <div class="col-lg-4">
-            
-                <img src={Roy} 
-            className=" rounded-circle w-50" />
-              <title>Placeholder</title>
-              <rect
-                width="100%"
-                height="100%"
-                fill="var(--bs-secondary-color)"
-              />
-            
-            <h2 class="fw-normal">Heading</h2>
-            <p>
-              And lastly this, the third column of representative placeholder
-              content.
-            </p>
-            <p>
-              <a class="btn btn-secondary" href="#">
-                View details &raquo;
-              </a>
-            </p>
-          </div>
-        </div> */}
+        
 
         <hr class="featurette-divider" />
 
         <div class="row featurette">
           <div class="col-md-7">
             <h2 class="featurette-heading fw-normal lh-1">
-              Wear the prototype.{" "}
-              <span class="text-body-secondary">Uncover What Your Sleep’s Been Hiding.</span>
+              Equip Yourself.{" "}
+              <span class="text-body-secondary">Unlock Affordable Sleep Apnea Monitoring.</span>
             </h2>
             <p class="lead">
-            Comfortable, low-profile gear built to capture real sleep apnea signals.
-            Just strap on the oximeter, nasal cannula, and chest belt—everything you need for clinical-grade monitoring at home.
+            Built with Arduino, a thermistor, flex sensor, and other cost-effective components, 
+            our prototype delivers accurate, real-time data for sleep apnea detection—bringing 
+            clinical insight to your bedroom without the clinical price tag.
             </p>
           </div>
           <div class="col-md-5">
-            <svg
-              class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+          <img
+              class="bd-placeholder-img bd-placeholder-img-lg featurette-image"
               width="500"
               height="500"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: 500x500"
-              preserveAspectRatio="xMidYMid slice"
-            >
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" />
-              <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">
-                500x500
-              </text>
-            </svg>
+              src={arduino}
+              
+            
+            />
           </div>
         </div>
 
@@ -115,8 +59,8 @@ function About() {
         <div class="row featurette">
           <div class="col-md-7 order-md-2">
             <h2 class="featurette-heading fw-normal lh-1">
-              Start the session.{" "}
-              <span class="text-body-secondary">Let the Data Tell the Story.</span>
+              Capture Every Breathe.{" "}
+              <span class="text-body-secondary">Every Drop.</span>
             </h2>
             <p class="lead">
             Continuous tracking that doesn’t miss a breath.
@@ -163,6 +107,86 @@ function About() {
         </div>
 
         <hr class="featurette-divider" />
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2">
+            <h2 class="featurette-heading fw-normal lh-1">
+              Capture Every Breathe.{" "}
+              <span class="text-body-secondary">Every Drop.</span>
+            </h2>
+            <p class="lead">
+            Continuous tracking that doesn’t miss a breath.
+            Once you begin, the system records your vitals every 250 milliseconds—breath patterns, oxygen drops, and subtle disruptions all in high resolution.
+            </p>
+          </div>
+          <div class="col-md-5 ">
+          <img
+              class="bd-placeholder-img bd-placeholder-img-lg featurette-image"
+              width="500"
+              height="500"
+              src={Pic2}
+              
+            
+            />
+          </div>
+        </div>
+
+        <hr class="featurette-divider" />
+
+
+        <h2 className="my-4 fw-bolder">The Team</h2>
+        <div class="row rounded-3 text-center">
+          <div class="col-lg-4 ">
+            <img src={enrico} 
+            className=" rounded-circle w-50" />
+              <title>Placeholder</title>
+              <rect
+                width="100%"
+                height="100%"
+                fill="var(--bs-secondary-color)"
+              />
+            
+            <h2 class="fw-normal">Enrico Velasquez</h2>
+            <p>
+            Enrico brings the design to life with detailed documentation and a custom 3D prototype.
+            </p>
+            
+          </div>
+          <div class="col-lg-4">
+            
+                <img src={Roy} 
+            className=" rounded-circle w-50" />
+              <title>Placeholder</title>
+              <rect
+                width="100%"
+                height="100%"
+                fill="var(--bs-secondary-color)"
+              />
+            
+            <h2 class="fw-normal">Rafael Dizon</h2>
+            <p>
+            Rafael leads the charge, engineering both the hardware and software that power the system.
+            </p>
+            
+          </div>
+          <div class="col-lg-4">
+            
+                <img src={seng} 
+            className=" rounded-circle w-50" />
+              <title>Placeholder</title>
+              <rect
+                width="100%"
+                height="100%"
+                fill="var(--bs-secondary-color)"
+              />
+            
+            <h2 class="fw-normal">Russel Samson</h2>
+            <p>
+            Russel makes sure the hardware runs reliably, helping assemble and refine the device.
+            </p>
+            
+          </div>
+        </div>
       </div>
     </div>
   );
